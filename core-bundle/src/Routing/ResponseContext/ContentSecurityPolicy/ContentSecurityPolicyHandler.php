ade289a0ac24132932ed5097776efe2d0fecd6a7
fritzmg <fmg@inspiredminds.at>
2023-12-18 21:55:49 +0000

    wip

--- core-bundle/src/Routing/ResponseContext/ContentSecurityPolicy/ContentSecurityPolicyHandler.php
+++ core-bundle/src/Routing/ResponseContext/ContentSecurityPolicy/ContentSecurityPolicyHandler.php
@@ -3,29 +3,24 @@
 namespace Contao\CoreBundle\Routing\ResponseContext\ContentSecurityPolicy;
 
 use Nelmio\SecurityBundle\ContentSecurityPolicy\DirectiveSet;
-use Nelmio\SecurityBundle\ContentSecurityPolicy\NonceGenerator;
-use Nelmio\SecurityBundle\ContentSecurityPolicy\PolicyManager;
 use Symfony\Component\HttpFoundation\Request;
 
 final class ContentSecurityPolicyHandler
 {
-    private DirectiveSet $directives;
     private bool $reportOnly = false;
     private string $nonce;
-    private string $scriptNonce;
-    private string $styleNonce;
     private array $signatures = [];
 
-    public function __construct(
-        private readonly PolicyManager $policyManager,
-        private readonly NonceGenerator $nonceGenerator,
-    ) {
-        $this->directives = new DirectiveSet($this->policyManager);
+    private static $validNonceDirectives = ['script-src', 'style-src', 'script-src-elem', 'style-src-elem'];
+    private static $validHashDirectives = ['script-src', 'script-src-elem', 'script-src-attr', 'style-src', 'style-src-elem', 'style-src-attr'];
+
+    public function __construct(private readonly DirectiveSet $directives)
+    {
     }
 
-    public function parseContentSecurityPolicyHeader(string $header): self
+    public function setReportOnly(bool $reportOnly): self
     {
-        // TODO
+        $this->reportOnly = $reportOnly;
 
         return $this;
     }
@@ -35,9 +30,9 @@ final class ContentSecurityPolicyHandler
         return $this->reportOnly;
     }
 
-    public function setReportOnly(bool $reportOnly): self
+    public function setDirectives(DirectiveSet $directives): self
     {
-        $this->reportOnly = $reportOnly;
+        $this->directives = $directives;
 
         return $this;
     }
@@ -49,46 +44,80 @@ final class ContentSecurityPolicyHandler
 
     public function getNonce(string $directive): ?string
     {
-        if (!\in_array($directive, ['script-src', 'style-src', 'script-src-elem', 'style-src-elem'], true)) {
+        if (!\in_array($directive, self::$validNonceDirectives, true)) {
             throw new \InvalidArgumentException('Invalid directive');
         }
 
-        if (!$this->directives->getDirective($directive)) {
+        if (!$this->isDirectiveSet($directive)) {
             return null;
         }
 
         if (!$this->nonce) {
-            $this->nonce = $this->nonceGenerator->generate();
+            $this->nonce = base64_encode(random_bytes(18));
         }
 
-        $this->signatures[$directive][] = $this->nonce;
+        $this->signatures[$directive][] = 'nonce-'.$this->nonce;
 
         return $this->nonce;
     }
 
-    public function getHash(string $directive, string $code): ?string
+    public function getHash(string $directive, string $script, string $algorithm = 'sha384'): ?string
     {
-        if (!\in_array($directive, ['script-src', 'script-src-elem', 'script-src-attr'], true)) {
+        if (!\in_array($directive, self::$validHashDirectives, true)) {
             throw new \InvalidArgumentException('Invalid directive');
         }
+
+        if (!$this->isDirectiveSet($directive)) {
+            return null;
+        }
+
+        $hash = base64_encode(hash($algorithm, $script, true));
+
+        $this->signatures[$directive][] = $algorithm.'-'.$hash;
+
+        return $hash;
     }
 
     /** 
-     * Adds a source for a directive, e.g. frame-src https://www.youtube.com/…
+     * Sets or appends a source for a directive, e.g. frame-src https://www.youtube.com/…
      * 
-     * @param $directive The directive for which the source should be added.
-     * @param $source The source for the directive.
-     * @param $autoIgnore Does not add the source if no directive is set yet.
+     * @param string $directive The directive for which the source should be added.
+     * @param string $source The source for the directive.
+     * @param bool $autoIgnore Does not add the source if no directive (or its fallback) is set yet.
      */
     public function addSource(string $directive, string $source, bool $autoIgnore = true): self
     {
-        $current = $this->directives->getDirective($directive);
-
-        if ($current || !$autoIgnore) {
+        if (!$this->isDirectiveSet($directive, true) || !$autoIgnore) {
+            $current = $this->directives->getDirective($directive);
             $this->directives->setDirective($directive, trim($current.' '.$source));
         }
 
         return $this;
+    }
+
+    /** 
+     * Checks if a directive or any of its fallbacks are set.
+     * 
+     * @param string $directive The directive.
+     * @param bool $includeFallback Whether to include the fallbacks of the directive in the check.
+     */
+    public function isDirectiveSet(string $directive, bool $includeFallback = true): bool
+    {
+        if ($this->directives->getDirective($directive)) {
+            return true;
+        }
+
+        if (!$includeFallback || (DirectiveSet::getNames()[$directive] ?? null) !== DirectiveSet::TYPE_SRC_LIST) {
+            return false;
+        }
+
+        switch ($directive) {
+            case 'script-src-attr':
+            case 'script-src-elem': return $this->isDirectiveSet('script-src', $includeFallback);
+            case 'style-src-attr':
+            case 'style-src-elem': return $this->isDirectiveSet('style-src', $includeFallback);
+            default: return $this->isDirectiveSet('default-src', $includeFallback);
+        }
     }
 
     public function buildHeaders(Request $request, bool $reportOnly, bool $compatHeaders): array
