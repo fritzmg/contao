6c67b26e11b419b08c49665c2d56de8c12044330
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2024-10-12 11:14:38 +0200

    fix DumperTest

--- core-bundle/tests/Doctrine/Backup/DumperTest.php
+++ core-bundle/tests/Doctrine/Backup/DumperTest.php
@@ -304,9 +304,18 @@ class DumperTest extends ContaoTestCase
         $calls = [];
         $returns = [];
 
+        $reflection = new \ReflectionClass(ArrayResult::class);
+        $dbal41 = count($reflection->getConstructor()->getParameters()) > 1;
+
         foreach ($queries as $query => $results) {
+            if ($dbal41) {
+                $result = new ArrayResult(array_keys($results[0] ?? []), $results);
+            } else {
+                $result = new ArrayResult($results);
+            }
+
             $calls[] = [$query];
-            $returns[] = new Result(new ArrayResult($results), $connection);
+            $returns[] = new Result($result, $connection);
         }
 
         $connection
